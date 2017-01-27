c611ffab780997ed33059484961597d6956c2840
Yury Koropenko <yurykoropenko@gmail.com>
2017-01-27 13:01:36 +0200

    commit 3

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	
+		/**/
+	$('.recomend__slider-prew').on('click', function(e) {
+		$('.recomend__slider .slick-prev').trigger('click', false);
+	});
+
+	$('.recomend__slider-next').on('click', function(e) {
+		$('.recomend__slider .slick-next').trigger('click', false);
+	});
+	/**/
 });
