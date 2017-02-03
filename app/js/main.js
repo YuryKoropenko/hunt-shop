8e73df2dffdf6a427afd4e9097f37d269f5e0a43
Yury Koropenko <yurykoropenko@gmail.com>
2017-02-03 17:11:06 +0200

    commit 8

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,14 @@ $(document).ready(function() {
 	$('.recomend__slider-next').on('click', function(e) {
 		$('.recomend__slider .slick-next').trigger('click', false);
 	});
+
 	/**/
+	$('.basket__link').on('click', function() {
+		$(this).parent().children('.basket__popup').fadeToggle();
+		return false;
+	});
+	$('.basket__popup-pp').on('click', function() {
+		$(this).parent().parent('.basket__popup').fadeOut();
+		return false;
+	})
 });
