69d0a12e302eb1c6fc914d89d1090d47ad5326f4
Yury Koropenko <yurykoropenko@gmail.com>
2017-02-04 12:46:55 +0200

    commit 9

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-		/**/
+/*Тригеры на слайдер*/
 	$('.recomend__slider-prew').on('click', function(e) {
 		$('.recomend__slider .slick-prev').trigger('click', false);
 	});
@@ -8,13 +8,32 @@ $(document).ready(function() {
 		$('.recomend__slider .slick-next').trigger('click', false);
 	});
 
-	/**/
+/*Корзина*/
 	$('.basket__link').on('click', function() {
 		$(this).parent().children('.basket__popup').fadeToggle();
+		$(document).click(function(event) {
+			if ($(event.target).closest('.basket__popup').length) return;
+				$('.basket__popup').fadeOut();
+				event.stopPropagation();
+			});
 		return false;
 	});
 	$('.basket__popup-pp').on('click', function() {
 		$(this).parent().parent('.basket__popup').fadeOut();
 		return false;
 	})
+
+/*Кнопка 'Выберите файл'*/
+	$(function() {
+		if($('#chose_file').length)
+		{
+			$('#chose_file').click(function() {
+				$('#chose_file_input').click();
+				return(false);
+			});
+			$('#chose_file_input').change(function() {
+				$('#chose_file_text').html($(this).val());
+			}).change();
+		}
+	});
 });
